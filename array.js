da28c7f43769a691a15f51e65774c02d3701ebb5
Eric Snell <eric_snell21@yahoo.com>
2016-08-18 14:48:45 -0400

    popping values

--- array.js
+++ array.js
@@ -6,11 +6,16 @@ let memory = require('./memory');
 class Array {
   constructor() {
     this.length = 0
+    this._capacity = 0
     this.ptr = memory.allocate(this.length)
   }
 
   push(value) {
-    this._resize(this.length + 1)
+    if (this.length >= this._capacity) {
+      // Each time you go over the capacity, you triple the size of allocated memory
+      this._resize((this.length + 1) * Array.SIZE_RATIO)
+    }
+
     memory.set(this.ptr + this.length, value)
     this.length++
   }
@@ -19,9 +24,30 @@ class Array {
     let oldPtr = this.ptr
     this.ptr = memory.allocate(size)
     if (this.ptr === null) {
-      throw new Error('out of memory');
+      throw new Error('out of memory')
     }
     memory.copy(this.ptr, oldPtr, this.length)
     memory.free(oldPtr)
+    this._capacity = size
   }
+
+  retrieve(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error')
+    }
+    return memory.get(this.ptr + index)
+  }
+
 }
+
+Array.SIZE_RATIO = 3;
+
+
+
+
+
+
+
+
+
+
