94502b55aea9a60aa7558f05d377f67d9a62f419
Eric Snell <eric_snell21@yahoo.com>
2016-08-18 17:30:18 -0400

    1st interview question algorithm added

--- Interview-questions.js
+++ Interview-questions.js
@@ -1,2 +1,12 @@
 // Imagine you have an array of numbers. Write an algorithm to remove all number less than five from the array.
+1. Loop through array
+2. Find values less than 5
+3. Remove values found using splice
+4. Return array
 
+		- or -
+
+1. Loop through array
+2. Find values greater than 4
+3. Push values to new array
+4. Return new array
